08d444af5e613111a40273a5b759052290c0dad4
javiermoralesdev <javiermorales2006@proton.me>
2026-05-07 13:19:37 +0200

    Added the pipes

--- scripts/flap_space/flap_space_player.gd
+++ scripts/flap_space/flap_space_player.gd
@@ -1,28 +1,54 @@
+class_name FlapSpacePlayer
 extends CharacterBody2D
 
 const JUMP_VELOCITY: float = -200.0
 const gravity: float = 5
 const FALL_THRESSHOLD: int = 100
+const BASE_SPEED: int = 100
+
+var pipe_speed: int = 100
+var points: int = 0
 
 @onready var jump_texture: Texture = preload("res://sprites/flap_space/Jump (32x32).png")
 @onready var fall_texture: Texture = preload("res://sprites/flap_space/Fall (32x32).png")
+@onready var pipe_prefab: PackedScene = preload("res://scenes/flap_space/pipe.tscn");
+
 var dead: bool = false 
 var start: bool = false
 
 func _physics_process(_delta: float) -> void:
+	var new_speed: int = BASE_SPEED + (points * 10)
+	pipe_speed = new_speed
+	if start and not dead:
+		%BackgroundTexture.material.set_shader_parameter("motion", Vector2(new_speed, 0))
+	
 	if not is_on_floor():
 		if start:
 			velocity.y += gravity
 	else:
-		dead = true
-		$PlayerSprite.modulate = Color.DARK_RED
+		die()
 		velocity.y = 0
 	
 	if Input.is_action_just_pressed("tap") and not dead:
 		velocity.y = JUMP_VELOCITY
-		start = true
+		if not start:
+			start = true
+			%SpawnTimer.start()
 	if velocity.y < 0:
 		$PlayerSprite.texture = jump_texture
 	if velocity.y > FALL_THRESSHOLD:
 		$PlayerSprite.texture = fall_texture
 	move_and_slide()
+
+func die() -> void:
+	pipe_speed = 0
+	dead = true
+	$PlayerSprite.modulate = Color.RED
+	%BackgroundTexture.material.set_shader_parameter("motion", Vector2.ZERO)
+
+
+func _on_spawn_timer_timeout() -> void:
+	var pipe_instance: Area2D = pipe_prefab.instantiate()
+	pipe_instance.position = Vector2(500, 0);
+	get_tree().root.add_child(pipe_instance)
+	print("Generación")
--- scripts/flap_space/pipe.gd
+++ scripts/flap_space/pipe.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
+@onready var player: FlapSpacePlayer
+
+func _ready() -> void:
+	position.y = randi_range(-70, 80)
+	
 
 func _process(delta: float) -> void:
-	position.x += -200*delta
+	if not %Player.start:
+		return
+	position.x += -%Player.pipe_speed*delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
@@ -11,3 +18,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_death_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is FlapSpacePlayer:
+		(body as FlapSpacePlayer).die()
